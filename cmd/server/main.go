75095215f7e00ccc59733e5c02f76ee530606a47
Denis Vakhrushev <norealpwnz@gmail.com>
2025-03-07 01:36:02 +0300

    add load config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/sunriseex/test_wallet/internal/config"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Приложение test_wallet запущено!")
+	})
+
+	addr := fmt.Sprintf(":%s", cfg.AppPort)
+	fmt.Printf("Запуск сервера на порту: %s", cfg.AppPort)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sunriseex/test_wallet
 
 go 1.24.1
+
+require github.com/joho/godotenv v1.5.1
